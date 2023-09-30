0b48d1aaec4df24a5c356d047615b24382313fa0
rahul <rahulrajput81680@gmail.com>
2023-09-30 18:30:24 +0530

    side bar added in all subpages

--- papers.js
+++ papers.js
@@ -16,6 +16,22 @@
       }
   }
 
+  // sidebar section starts 
+
+
+const menuIcon = document.getElementById('menu-icon');
+const sidebar = document.getElementById('sidebar');
+const closeIcon = document.getElementById('close-icon');
+
+menuIcon.addEventListener('click', () => {
+  sidebar.classList.add('active');
+});
+
+closeIcon.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+});
+
+
 
 // semester 1
 
