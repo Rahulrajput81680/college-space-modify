fdf3d0f5d631313a02dbc4facbc86f752f38599f
shubhagarwal <shubh342002@gmail.com>
2024-01-13 21:01:31 +0530

    copyright add

--- papers.js
+++ papers.js
@@ -55,9 +55,9 @@ document.getElementById('downloadButton1').addEventListener('click', function ()
     
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
-        '2020-21': './papers/MCA Int/MCA Int 1st Sem (2020-21).pdf',
-        '2021-22': './papers/MCA Int/MCA Int 1st Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 1st Sem (2022-23).pdf',
+        '2020-21': 'https://mega.nz/file/IjF3ATpR#jOqdlLGkdIvlVFfvMHA5SgBzMj42oTB9-Y_Pjpw469o',
+        '2021-22': 'https://mega.nz/file/Nq12wRzb#08q_xho2RYU8TakfCxR0gkjZW_7buhJ33EkkiYqOvy0',
+        '2022-23': 'https://mega.nz/file/p3EgHCZQ#kn1nESic7NfFRBfnRLcAkymKGq5xYe5GS5mqMVHb5uM',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -80,8 +80,8 @@ document.getElementById('downloadButton2').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 2nd Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 2nd Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/x6d2VIoS#oyqbNnwhnZOR09hdojfD9i8zekATwJurBA435HbeD6o',
+        '2022-23': 'https://mega.nz/file/0nNSxbwa#tGPnkZHKH9PNaGqBG2jkJutLPEAXUIw9kY1I912iC7E',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -101,8 +101,8 @@ document.getElementById('downloadButton3').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 3rd Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 3rd Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/ljtmATjR#aakD_l3-KyjXgHNLcjr0WSWTj8F7CrEOOUZCVyU_rt8',
+        '2022-23': 'https://mega.nz/file/NrEQBJ4Z#hjad0jT64IXiF8CqIrNIZibckBmcCjNq6GfV22f0sf8',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -122,8 +122,8 @@ document.getElementById('downloadButton4').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 4th Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 4th Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/hiclCIZT#TwCe1Mk0yMrBKFaCKzQlJujcjDDyTeqTdFxR4RV2j0s',
+        '2022-23': 'https://mega.nz/file/Z6kRmB6Q#wXPVaK-nAVzF8UIqLwIv9AjRINAt1CVuWyX3z-aXwes',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -143,8 +143,8 @@ document.getElementById('downloadButton5').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 5th Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 5th Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/wuMlxJ4S#oNE5gOiDrWoREhD5L7yDrIMuiHUgEAbP0vkJbU2ZQnE',
+        '2022-23': 'https://mega.nz/file/82UFzTQJ#S-k8Ddu-ofSqAJuHsteIiEBBKMNDHLxt8G30yuOUUH4',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -164,8 +164,8 @@ document.getElementById('downloadButton6').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 6th Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 6th Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/EuFxTQpY#PZHXsIOefxX3DROQk8q6GObbiXukV9u9TX6T9JUuEbU',
+        '2022-23': 'https://mega.nz/file/82N1XK4Y#qdqI60KBxEKSSuEJytAbR-ICOt5nMpOG4lhledu-9x8',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -185,8 +185,8 @@ document.getElementById('downloadButton7').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 7th Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 7th Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/RyMXFDDJ#Hd3FYco3SYMEr4LvKvt5r1_SElgmzrqLcbISVvijk_s',
+        '2022-23': 'https://mega.nz/file/EmcAyQCL#HyYvoLQSjvoYfOLpWaQaNeI31l70p7bRoUtZ2tlciaQ',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -207,8 +207,8 @@ document.getElementById('downloadButton8').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 8th Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 8th Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/428zBITK#yL8BCzWtmC75Mo4VvCk2b8oRkc-SfteZ82txppCWh6A',
+        '2022-23': 'https://mega.nz/file/Iy9i0DJC#KelaevbUM5VSiFYXfY_roIF9zfOeut4EvemST3efbxw',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -228,8 +228,8 @@ document.getElementById('downloadButton9').addEventListener('click', function ()
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
         // '2019-20': './papers/',
-        '2021-22': './papers/MCA Int/MCA Int 9th Sem (2021-22).pdf',
-        '2022-23': './papers/MCA Int/MCA Int 9th Sem (2022-23).pdf',
+        '2021-22': 'https://mega.nz/file/hyF2FLRY#xtJYgF4FVMRCGpxNpKBLIdC-4SuOD1issZYov2zmsBU',
+        '2022-23': 'https://mega.nz/file/pnVVUDJa#XUpNAvThkmnhiGkbhENlnZYIX3pogrFUrL42YtC9LSg',
     };
 
     if (curriculumFiles[selectedYear]) {
@@ -543,14 +543,15 @@ document.getElementById('mca-downloadButton1').addEventListener('click', functio
 
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
-        '2021-22': 'https://mega.nz/file/8mUlnZpY#gpsn7WRXoZTDl4XJeDjZ2Ca4Ergsykqu_jr2By41taM',
+        '2021-22': 'https://mega.nz/file/4qlgDCzK#q08FYWuqBc9YGcwvHZcs_jLx-c9eB-4p5FytcSPQUGA',
+        '2022-23': 'https://mega.nz/file/o6UCGADA#OwfDoIX7HYXi-kLPR2FmtvhmfoVXmcnPGhpdYX9DeFk',
     };
 
     if (curriculumFiles[selectedYear]) {
         window.open(curriculumFiles[selectedYear], '_blank');
     } else {
       
-        alert('Will upload soon');
+        alert('coming soon');
     }
 });
 
@@ -563,14 +564,15 @@ document.getElementById('mca-downloadButton2').addEventListener('click', functio
 
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
-        '2021-22': 'https://mega.nz/file/suFCTJoL#arVnV4yUsLbb6PKgmQO6vZCkQt3aXGdcPVFfJyKKtDw',
+        '2021-22': 'https://mega.nz/file/E28jXZjJ#QI3AIThLsjVBQoHn-2x-RUw1732hEWP_onBIpzF_Wpk',
+        '2022-23': 'https://mega.nz/file/gycjkCLY#8a8lveP26eJK0jr3cuk39vzdpfxrO0DM1srVc9OFzxM',
     };
 
     if (curriculumFiles[selectedYear]) {
         window.open(curriculumFiles[selectedYear], '_blank');
     } else {
        
-        alert('Will upload soon');
+        alert('coming soon');
     }
 });
 
@@ -583,14 +585,15 @@ document.getElementById('mca-downloadButton3').addEventListener('click', functio
 
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
-        '2021-22': 'https://mega.nz/file/1y81VAYa#ty-TvcJLyDN8oCYBbjh4wu9rfS2T-KJQnEit0R3mtgc',
+        '2021-22': 'https://mega.nz/file/4n03VTxB#LgghiaGHxr2x-B7CCiHIh8S_4nfD1BslGeSPm1AGXwc',
+        '2022-23': 'https://mega.nz/file/IzNjxaDY#jHGZ5UBukoersnirOzX4MTofAgjmvgaCMftK704Y9yk',
     };
 
     if (curriculumFiles[selectedYear]) {
         window.open(curriculumFiles[selectedYear], '_blank');
     } else {
        
-        alert('Will upload soon');
+        alert('coming soon');
     }
 });
 
@@ -603,6 +606,8 @@ document.getElementById('mca-downloadButton4').addEventListener('click', functio
 
     // Define the curriculum file URLs for each year
     const curriculumFiles = {
+        '2021-22': 'https://mega.nz/file/4mlD2IJC#7tzaAFhppiC0ZMTbUBUxEf3Wpfr_7MGLU1es-P1apVA',
+        '2022-23': 'https://mega.nz/file/gm0SwIhZ#eSncwTNNPF50PHMfgtC-EmgkVr4PP6EHDRNWbZ_MiEE',
         // '2022-23': './papers/BCA.pdf',
     };
 
@@ -610,7 +615,7 @@ document.getElementById('mca-downloadButton4').addEventListener('click', functio
         window.open(curriculumFiles[selectedYear], '_blank');
     } else {
        
-        alert('Will upload soon');
+        alert('coming soon');
     }
 });
 
